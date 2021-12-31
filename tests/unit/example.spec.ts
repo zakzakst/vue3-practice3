3f7ca036d8902a0ec7c5da987ac95b0be861e69f
zakzakst <59270506+zakzakst@users.noreply.github.com>
2021-12-31 20:24:12 +0900

    STEP8-3

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import HomeComponent from '@/views/Home.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+describe('Home.vue', () => {
+  it('renders text when passed', () => {
+    const text = 'ホーム';
+    const wrapper = shallowMount(HomeComponent);
+    expect(wrapper.text()).toMatch(text);
   });
 });

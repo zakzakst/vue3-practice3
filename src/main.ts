9abdc89f6c529e4ef85aa34b8b7a68dec53295fb
zakzakst <59270506+zakzakst@users.noreply.github.com>
2022-01-01 11:13:32 +0900

    STEP10-1

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import VueCompositionAPI from '@vue/composition-api';
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
+import vuetify from '@/plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.use(VueCompositionAPI);
 
@@ -11,5 +14,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');

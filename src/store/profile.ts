0133f39ff58b12e3f527817186c13e7eea4979b3
zakzakst <59270506+zakzakst@users.noreply.github.com>
2022-01-01 11:31:14 +0900

    STEP10-2

---
+++ src/store/profile.ts
@@ -0,0 +1,10 @@
+import { Profile } from '@/store/profile.model';
+
+export const profileMockData: Profile = {
+  userId: '66006b29-727e-4ed8-a3c8-95d4438f66d4',
+  userName: 'naminami',
+  nickname: '波平',
+  themeColor: '#4caf50',
+  hasAvatar: false,
+  mailAddress: 'namihei@example.com',
+};
---
+++ src/store/shared-user.ts
@@ -0,0 +1,36 @@
+import { SharedUser } from '@/store/shared-user.model';
+
+export const sharedUserMockData: SharedUser[] = [
+  {
+    userId: '66006b29-727e-4ed8-a3c8-95d4438f66d4',
+    userName: 'naminami',
+    nickname: '波平',
+    themeColor: '#4caf50',
+    hasAvatar: false,
+    display: true,
+  },
+  {
+    userId: '2ec8d984-aa5f-4f7e-b1a8-c9e478b54ffe',
+    userName: 'fune',
+    nickname: 'フネ',
+    themeColor: '#ff9800',
+    hasAvatar: false,
+    display: true,
+  },
+  {
+    userId: '58e4eb36-2be9-4448-b7f9-4603fd1fd026',
+    userName: 'katuo',
+    nickname: 'カツオ',
+    themeColor: '#2196f3',
+    hasAvatar: false,
+    display: true,
+  },
+  {
+    userId: '74ecde04-e90a-4b8a-ad3e-aa4dffac6127',
+    userName: 'wakame',
+    nickname: 'ワカメ',
+    themeColor: '#e91e63',
+    hasAvatar: false,
+    display: true,
+  },
+];

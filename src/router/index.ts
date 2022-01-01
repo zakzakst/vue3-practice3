580078308516f669a867d45453691bd6cefbbf2a
zakzakst <59270506+zakzakst@users.noreply.github.com>
2022-01-01 10:08:04 +0900

    STEP9-2

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,39 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
 import HomeComponent from '@/views/Home.vue';
+import CalendarComponent from '@/views/Calendar.vue';
+import ProfileComponent from '@/views/Profile.vue';
+import ShareComponent from '@/views/Share.vue';
+import SignInComponent from '@/views/SignIn.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: HomeComponent,
+  },
+  {
+    path: '/calendar/:type',
+    name: 'calendar',
+    component: CalendarComponent,
+    props: true,
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: '/share',
+    name: 'share',
+    component: ShareComponent,
+  },
+  {
+    path: '/sign-in',
+    name: 'sign-in',
+    component: SignInComponent,
   },
 ];
 

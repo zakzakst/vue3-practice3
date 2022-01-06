62628e3e9c9f104406571397b30c931161e48dc6
zakzakst <59270506+zakzakst@users.noreply.github.com>
2022-01-06 23:04:33 +0900

    STEP10-4-5

--- src/store/calendar-event.model.ts
+++ src/store/calendar-event.model.ts
@@ -15,3 +15,13 @@ export interface CalendarEventTodayDetail extends CalendarEventDetailBase {
   startTime: string | null;
   endTime: string | null;
 }
+
+export interface NewCalendarEvent {
+  id: string | null;
+  userId: string | null;
+  name: string | null;
+  start: string;
+  end: string | null;
+  memo: string | null;
+  shared: boolean;
+}

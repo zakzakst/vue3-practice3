1964d65ba74594e16f026a56c85127a983d866c7
zakzakst <59270506+zakzakst@users.noreply.github.com>
2022-01-09 00:09:53 +0900

    STEP10-4-5

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -404,6 +404,192 @@ export default defineComponent({
       setToday: () => {
         state.focus = state.today;
       },
+      // カレンダーを1つ前に戻します。
+      prev: () => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        state.calendar!.prev();
+      },
+      // カレンダーを1つ先に進めます。
+      next: () => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        state.calendar!.next();
+      },
+      // イベントの色を取得します。
+      getEventColor: (event: CalendarEvent) => {
+        if (!event) {
+          return;
+        }
+        return getThemeColor(event.userId);
+      },
+      // カレンダーに表示する日のフォーマットを行います。
+      formatDay: (timestamp: CalendarTimestamp) => {
+        return new Date(timestamp.date).getDate();
+      },
+      // カレンダーに表示する月のフォーマットを行います。
+      formatMonth: (timestamp: CalendarTimestamp) => {
+        return `${new Date(timestamp.date).getMonth() + 1} /`;
+      },
+      // イベント詳細を表示します。
+      showEvent: ({
+        nativeEvent,
+        event,
+      }: {
+        nativeEvent: Event;
+        event: CalendarEvent;
+      }) => {
+        // 実装は https://vuetifyjs.com/ja/components/calendars/#イベント 参照。
+        // setTimeoutを使用して遅延させないと、メニューを正しく表示できない。
+        const open = () => {
+          state.selectedEvent = event;
+          state.menuActivatorElement = nativeEvent.target as HTMLInputElement;
+          setTimeout(() => {
+            state.isOpenEventMenu = true;
+          }, 10);
+        };
+        if (state.isOpenEventMenu) {
+          state.isOpenEventMenu = false;
+          setTimeout(open, 10);
+        } else {
+          open();
+        }
+        // クリックイベントの伝搬を抑止します。
+        nativeEvent.stopPropagation();
+      },
+      // カレンダー（日）を表示します。
+      viewDay: ({ date }: { date: string }) => {
+        state.focus = date;
+        context.root.$router.push('/calendar/day');
+      },
+      // イベントの新規登録を開始します。
+      startAddEvent: ({ date }: { date: string }) => {
+        state.newEvent = {
+          id: null,
+          userId: null,
+          name: null,
+          start: date,
+          end: null,
+          memo: null,
+          shared: true,
+        };
+        state.startDate = parseTimestamp(state.newEvent.start).date;
+        state.isOpenEventDialog = true;
+        // クリックイベントの伝搬を停止します。
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        event!.stopPropagation();
+      },
+      /**
+       * カレンダー上部の年月を表示するために使用する、表示範囲を更新します。
+       * カレンダーが描画されたタイミング、または表示範囲が変更されたタイミングで呼び出されます。
+       */
+      updateRange: ({
+        start,
+        end,
+      }: {
+        start: CalendarTimestamp;
+        end: CalendarTimestamp;
+      }) => {
+        state.start = start;
+        state.end = end;
+      },
+      // 自分のイベントではないことを判定します。
+      isNotOwner: (userId: string) => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        return userId !== profileStore.profile!.userId;
+      },
+      // イベントの編集を開始します。
+      startEditEvent: () => {
+        if (!state.selectedEvent) {
+          return;
+        }
+        const startTimestamp = parseTimestamp(state.selectedEvent.start);
+        state.startDate = startTimestamp.date;
+        state.startTime = startTimestamp.time;
+        if (state.selectedEvent.end) {
+          const endTimestamp = parseTimestamp(state.selectedEvent.end);
+          state.endDate = endTimestamp.date;
+          state.endTime = endTimestamp.time;
+        }
+        state.newEvent = { ...state.selectedEvent } as NewCalendarEvent;
+        state.isOpenEventMenu = false;
+        state.isOpenEventDialog = true;
+      },
+      // イベント日付選択ダイアログに表示する日のフォーマットを行います。
+      formatDayForDatePicker: (date: string) => {
+        return new Date(date).getDate();
+      },
+      // イベント開始日付選択ダイアログを閉じます。
+      closeStartDatePicker: () => {
+        state.isOpenStartDatePicker = false;
+      },
+      // 日付／時刻選択ダイアログにバインドしている値を初期化します。
+      clearPickerBindValue: () => {
+        state.startDate = null;
+        state.startTime = null;
+        state.endDate = null;
+        state.endTime = null;
+      },
+      // カレンダーを再描画します。
+      refresh: () => {
+        state.visible = false;
+        nextTick(() => {
+          state.visible = true;
+        });
+      },
+      // イベントの変更内容を保存します。
+      save: () => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const newEvent = state.newEvent!;
+        newEvent.start = !state.startTime
+          ? `${state.startDate}`
+          : `${state.startDate} ${state.startTime}`;
+        // 終了日は必須でない
+        if (state.endDate) {
+          newEvent.end = !state.endTime
+            ? `${state.endDate}`
+            : `${state.endDate} ${state.endTime}`;
+        }
+        if (!newEvent.id) {
+          add(newEvent);
+        } else {
+          update(newEvent);
+        }
+        methods.clearPickerBindValue();
+        state.isOpenEventDialog = false;
+        methods.refresh();
+      },
+      // イベント開始時刻の値をクリアします。
+      clearStartTime: () => {
+        state.startTime = null;
+        // イベント終了時刻のみの入力はできないため、イベント終了時刻もクリアする。
+        state.endTime = null;
+      },
+      // イベント開始時刻選択ダイアログを閉じます。
+      closeStartTimePicker: () => {
+        state.isOpenStartTimePicker = false;
+      },
+      // イベント終了日付の値をクリアします。
+      clearEndDate: () => {
+        state.endDate = null;
+        // イベント終了時刻もクリアする。
+        state.endTime = null;
+      },
+      // イベント終了日付選択ダイアログを閉じます。
+      closeEndDatePicker: () => {
+        state.isOpenEndDatePicker = false;
+      },
+      // イベント終了時刻の値をクリアします。
+      clearEndTime: () => {
+        state.endTime = null;
+      },
+      // イベント終了時刻選択ダイアログを閉じます。
+      closeEndTimePicker: () => {
+        state.isOpenEndTimePicker = false;
+      },
+      // イベント登録／編集ダイアログを閉じます。
+      closeEventDialog: () => {
+        methods.clearPickerBindValue();
+        state.isOpenEventDialog = false;
+      },
     };
 
     return {

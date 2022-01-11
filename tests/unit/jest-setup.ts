9e248b65efd3de7de5c67edb0f96047707f2320f
zakzakst <59270506+zakzakst@users.noreply.github.com>
2022-01-11 11:26:34 +0900

    STEP20-2

--- tests/unit/jest-setup.ts
+++ tests/unit/jest-setup.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // モック化対象のオブジェクト
 import * as veeValidate from 'vee-validate';
+import axios from 'axios';
 declare let global: any;
 
 global.mockVeeValidate = (valid: boolean) => {
@@ -8,4 +9,14 @@ global.mockVeeValidate = (valid: boolean) => {
   jest
     .spyOn(veeValidate, 'validate')
     .mockResolvedValue({ valid: valid } as any);
+};
+
+global.mockAxiosGet = (value: any) => {
+  // axios.getをモック化
+  jest.spyOn(axios, 'get').mockResolvedValue(value);
+};
+
+global.mockAxiosPatch = (value: any) => {
+  // axios.patchをモック化
+  jest.spyOn(axios, 'patch').mockResolvedValue(value);
 };
